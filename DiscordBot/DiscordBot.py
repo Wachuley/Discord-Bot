40a7fc27211c105da166d22675895e914fd12ebf
Mabel <dmabelmb04@gmail.com>
2025-10-20 16:34:37 -0600

    Comprobando el token

--- DiscordBot/DiscordBot.py
+++ DiscordBot/DiscordBot.py
@@ -1,1 +1,11 @@
-print("Hello world")
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+# Cargar el archivo .env con el token del bot
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+print(f"Token cargado: {TOKEN}")
